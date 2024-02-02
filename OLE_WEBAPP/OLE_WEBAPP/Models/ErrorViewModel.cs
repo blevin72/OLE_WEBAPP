b72e3a7d270490995b653095986d0162de57bc57
blevinsky91 <136006635+blevinsky91@users.noreply.github.com>
2024-02-02 10:46:14 -0500

    first commit

---
+++ OLE_WEBAPP/OLE_WEBAPP/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace OLE_WEBAPP.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
