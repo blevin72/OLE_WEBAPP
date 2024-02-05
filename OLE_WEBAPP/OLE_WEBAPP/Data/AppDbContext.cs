553b65c8a6f1e5cf5f2488d4d88299597263189b
blevinsky91 <136006635+blevinsky91@users.noreply.github.com>
2024-02-05 18:34:27 -0500

    configured entity framework and dbcontext

---
+++ OLE_WEBAPP/OLE_WEBAPP/Data/AppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace OLE_WEBAPP.Data
+{
+	public class AppDbContext : DbContext
+	{
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var connectionString = "Server=localhost:8889;Database=UnityAccess;User=root;Password=root";
+
+            var serverVersion = new MySqlServerVersion(new Version(5, 7, 39));
+            optionsBuilder.UseMySql(connectionString, serverVersion);
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+}
+
--- OLE_WEBAPP/OLE_WEBAPP/Program.cs
+++ OLE_WEBAPP/OLE_WEBAPP/Program.cs
@@ -1,4 +1,7 @@
-﻿var builder = WebApplication.CreateBuilder(args);
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
